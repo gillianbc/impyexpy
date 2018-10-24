01a10026450c7039c0cf0bc67b2d79380d75f600
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-10-24 16:53:36 +0100

    Added comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import fs from 'fs';
+// So we're just giving the imports from ./util/index.js a localname of utils
 import utils from './util';
 console.log('Hello World');
 
+//Run a named function. We could have read this name from a config file somewhere
 utils['cranberrysauce']();
